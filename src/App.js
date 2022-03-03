f8ca171fca35b3714aa223695489f47bdd172fed
luizfernandorg <luizfernandorg@gmail.com>
2022-03-03 18:01:49 -0300

    Adjust jshint esversion for ES6

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//jshint esversion:6
 import logo from './logo.svg';
 import './App.css';
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+//jshint esversion:6
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
