6a06700aa4962351b52d761220dbffb75e4d07a7
luizfernandorg <luizfernandorg@gmail.com>
2022-03-03 19:55:48 -0300

    A new way to create a new JSX using arrow function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Link from "./Link.component"
 
-function Greeting() {
-  return <h1><Link link="http://google.com"/> This is Fernando and this is my first component</h1>
-}
+// function Greeting() {
+//   return <h1><Link link="http://google.com"/> This is Fernando and this is my first component</h1>
+// }
+// const Greeting = () => React.createElement("h1", {}, "Hello World")
+const Greeting = () => (<div> 
+                          <h1>Hello World!</h1>
+                        </div>)
+
 ReactDOM.render(<Greeting/>, document.getElementById("root"))
